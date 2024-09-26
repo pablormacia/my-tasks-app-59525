c33d9c82a51d4a981adffaf2804ddf76ffe6f8ff
pablormacia <pr.macia@gmail.com>
2024-09-25 21:34:28 -0300

    Clase 4 - useState, spread, map, key

--- App.jsx
+++ App.jsx
@@ -1,82 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import {  Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react'
 
 export default function App() {
+  const [taskInput, setTaskInput] = useState("")
+  const [tasksList, setTasksList] = useState([])
+
+  //console.log("taskInput: ",taskInput)
+  //console.log("TasksList: ", tasksList)
+
+  const handlerAddItemToList = () => {
+    //setTask([...tasksList, setTasksList])
+    setTasksList(prevTasksList => [...prevTasksList,{"id":Math.random(),"value":taskInput}])
+  }
+
+  console.log("TasksList: ", tasksList)
+
   return (
     <>
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Añade una tarea' />
-        <Button title="+" color="#1D9225" />
-      </View>
-      <View style={styles.tasksContainer}>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder='Añade una tarea'
+            onChangeText={(text) => { setTaskInput(text) }}
+          />
+          <Button title="+" color="#1D9225" onPress={handlerAddItemToList} />
+        </View>
+        <View style={styles.tasksContainer}>
           <Text style={styles.title}>Tareas pendientes:</Text>
-          <View style={styles.taskContainer}>
-            <Text style={styles.taskText}>Tarea de prueba</Text>
-            <Button title="x" color="#C71919" />
-          </View>
-          <View style={styles.taskContainer}>
-            <Text style={styles.taskText}>Tarea de prueba</Text>
-            <Button title="x" color="#C71919" />
-          </View>
-          <View style={styles.taskContainer}>
-            <Text style={styles.taskText}>Tarea de prueba</Text>
-            <Button title="x" color="#C71919" />
-          </View>
-          <View style={styles.taskContainer}>
-            <Text style={styles.taskText}>Tarea de prueba</Text>
-            <Button title="x" color="#C71919" />
-          </View>
+          {
+            tasksList.map((task) => (
+              <View key={task.id} style={styles.taskContainer}>
+                <Text style={styles.taskText}>{task.value}</Text>
+                <Button title="x" color="#C71919" />
+              </View>
+            ))
+          }
+        </View>
       </View>
-    </View>
-    <StatusBar style="auto" />
+      <StatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:"#464646",
-    paddingHorizontal:20,
+  container: {
+    flex: 1,
+    backgroundColor: "#464646",
+    paddingHorizontal: 20,
   },
-  inputContainer:{
-    flexDirection:'row',
-    paddingTop:60,
-    paddingBottom:40,
+  inputContainer: {
+    flexDirection: 'row',
+    paddingTop: 60,
+    paddingBottom: 40,
     justifyContent: 'space-between',
     borderBottomColor: "#FFF",
     borderBottomWidth: 2,
   },
-  textInput:{
-    borderWidth:1,
-    borderColor:"#ccc",
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
     paddingVertical: 5,
-    paddingHorizontal:10,
+    paddingHorizontal: 10,
     borderRadius: 15,
     backgroundColor: "#ccc",
-    width:'90%'
+    width: '90%'
   },
-  tasksContainer:{
-    paddingTop:15,
-    gap:20,
+  tasksContainer: {
+    paddingTop: 15,
+    gap: 20,
   },
-  title:{
-    color:"#fff",
-    fontWeight:"bold",
-    alignSelf:"center",
-    fontSize:16
+  title: {
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    fontSize: 16
   },
-  taskContainer:{
-    flexDirection:'row',
-    justifyContent:'space-between',
+  taskContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: "center",
     paddingBottom: 8,
     borderBottomColor: "#ccc",
     borderBottomWidth: 1,
   },
-  taskText:{
-    color:"#fff"
+  taskText: {
+    color: "#fff"
   }
 })
 
