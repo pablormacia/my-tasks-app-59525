4f72a1b547916e9f6d64815c55a78711f0b65937
pablormacia <pr.macia@gmail.com>
2024-09-25 22:44:21 -0300

    Clase 4 - Modal, eliminar tasks

--- App.jsx
+++ App.jsx
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, FlatList, Modal,Pressable } from 'react-native';
 import { useState } from 'react'
 
 export default function App() {
   const [taskInput, setTaskInput] = useState("")
   const [tasksList, setTasksList] = useState([])
+  const [modalVisible, setModalVisible] = useState(false)
+  const [taskSelected, setTaskSelected] = useState({})
 
   //console.log("taskInput: ",taskInput)
   //console.log("TasksList: ", tasksList)
 
   const handlerAddItemToList = () => {
     //setTask([...tasksList, setTasksList])
-    setTasksList(prevTasksList => [...prevTasksList,{"id":Math.random(),"value":taskInput}])
+    setTasksList(prevTasksList => [...prevTasksList, { "id": Math.random(), "value": taskInput }])
+    setTaskInput("")
   }
 
-  console.log("TasksList: ", tasksList)
+  const handleDeleteTask = ()=>{
+    setTasksList(tasksList.filter((task)=>task.id != taskSelected.id))
+    setModalVisible(false)
+  }
+
+  const handleSelectedTask = (item)=>{
+    setTaskSelected(item)
+    console.log(item)
+    setModalVisible(true)
+  }
+
+  const renderTaskItem = ({ item }) => {
+    return (
+      <View style={styles.taskContainer}>
+        <Text style={styles.taskText}>{item.value}</Text>
+        <Button title="x" color="#C71919" onPress={()=>handleSelectedTask(item)} />
+      </View>
+    )
+  }
+
 
   return (
     <>
@@ -24,22 +46,60 @@ export default function App() {
             style={styles.textInput}
             placeholder='Añade una tarea'
             onChangeText={(text) => { setTaskInput(text) }}
+            value={taskInput}
           />
           <Button title="+" color="#1D9225" onPress={handlerAddItemToList} />
         </View>
         <View style={styles.tasksContainer}>
           <Text style={styles.title}>Tareas pendientes:</Text>
-          {
+          {/* {
             tasksList.map((task) => (
               <View key={task.id} style={styles.taskContainer}>
                 <Text style={styles.taskText}>{task.value}</Text>
                 <Button title="x" color="#C71919" />
               </View>
             ))
-          }
+          } */}
+          <FlatList
+            data={tasksList}
+            keyExtractor={item => item.id}
+            renderItem={renderTaskItem}
+          />
         </View>
       </View>
       <StatusBar style="auto" />
+      <Modal
+        animationType='fade'
+        visible={modalVisible}
+      >
+        <View style={styles.modalContainer}>
+          <Pressable style={styles.closeButton} onPress={()=>setModalVisible(false)}>
+            <Text style={styles.closeText}>x</Text>
+          </Pressable>
+          <View style={styles.textsContainer}>
+            <Text style={styles.modalTitle}>Confirmar eliminación</Text>
+            <Text style={styles.modalText}>{taskSelected.value}</Text>
+            <Text style={styles.modalDeleteTextWarning}>
+              Esta acción no se puede deshacer
+            </Text>
+          </View>
+
+          <View style={styles.buttonsContainer}>
+            <Pressable
+              style={styles.cancelBtn}
+              onPress={()=>setModalVisible(false)}
+            >
+              <Text style={styles.cancelText}>Cancelar</Text>
+            </Pressable>
+            <Pressable
+              style={styles.deleteBtn}
+              onPress={handleDeleteTask}
+            >
+              <Text style={styles.deleteText}>Si, eliminar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </>
   );
 }
@@ -87,7 +147,66 @@ const styles = StyleSheet.create({
   },
   taskText: {
     color: "#fff"
-  }
+  },
+  modalContainer: {
+    backgroundColor: "#464646",
+    flex: 1,
+  },
+  closeButton:{
+    alignSelf: 'flex-end',
+    padding:30
+  },
+  closeText:{
+    color:"#fff",
+    fontSize:30
+  },
+  textsContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  modalTitle: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  modalText: {
+    color: "#fff",
+    fontSize: 14,
+  },
+  modalDeleteTextWarning: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+  buttonsContainer: {
+    padding: 30,
+    flexDirection:'row',
+    gap:10
+  },
+  deleteBtn: {
+    backgroundColor: "#c71919",
+    width: "48%",
+    padding: 10,
+    borderRadius: 15,
+  },
+  deleteText: {
+    textAlign: "center",
+    fontSize: 14,
+    color: "#fff",
+    fontWeight:'bold'
+  },
+  cancelBtn: {
+    backgroundColor: "#C5C5C5",
+    width: "48%",
+    padding: 10,
+    borderRadius: 15,
+  },
+  cancelText: {
+    textAlign: "center",
+    fontSize: 14,
+    color: "#474646",
+  },
 })
 
 
